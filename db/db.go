1f35f77c12c17c3edabbded028f4e228562ebec6
3dw1nM0535 <workockmoses@gmail.com>
2019-09-28 13:56:23 +0300

    go-graphql-boilerplate

---
+++ db/db.go
@@ -0,0 +1,23 @@
+package db
+
+import (
+	"github.com/jinzhu/gorm"
+	// gorm postgres dialect
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+)
+
+// DB *grom.DB
+type DB struct {
+	*gorm.DB
+}
+
+// ConnectDB : connecting DB
+func ConnectDB() (*DB, error) {
+	db, err := gorm.Open("postgres", "host=localhost port=5432 user=demo dbname=rentr password=demo1234 sslmode=disable")
+
+	if err != nil {
+		panic(err)
+	}
+
+	return &DB{db}, nil
+}
